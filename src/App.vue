08307498867d43c2ac13572e40f8beb97423839f
Aristos Aresti <aristos.a@kpaxmarketing.com>
2025-08-19 17:14:02 +0300

    Update HelloWorld component message in App.vue

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ const getName = async () => {
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="You did it 65!" />
       <button class="green" @click="getName" aria-label="get name">
         Name from API is: {{ name }}
       </button>
